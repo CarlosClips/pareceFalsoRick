d8cae9f2ec3a791be302cf8d26e5910fde9e8cb2
Manoelpp <manoelpaiva725@gmail.com>
2026-07-30 10:16:26 -0300

    V1 - adicionado o menu interativo do programa

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Olá, este é o programa PareceFalso!");
+        System.out.println("Este programa tem o intuito de te ajudar e auxiliar para não cair em golpes \n");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int opcao = 0;
+        while (opcao != 4) {
+            System.out.println("=== MENU ===");
+            System.out.println("1 - Identificar mensagem suspeita de golpe \n" +
+                    "2 - Identificar URL suspeita de golpe \n" +
+                    "3 - Dicas de como não cair em mensagens fraudulentas \n" +
+                    "4 - Sair do programa \n");
+
+            System.out.print("Digite uma opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Opção 1 - Indentificar mensagem suspeita de golpe");
+                    System.out.print("Digite a mensagem que você acha suspeita: ");
+                    String mnsg = scanner.next();
+
+
+                case 2:
+                    System.out.println("Opção 2 - Indentificar URL suspeita de golpe");
+                    System.out.print("Digite a URL que você acha suspeita: ");
+                    String url = scanner.next();
+
+                case 3:
+                    System.out.println("Opção 3 - Dicas para não em mensagens fraudulentas");
+                    System.out.println("Desconfie de urgência excessiva, verifique links e canais \n" +
+                            "oficiais, e jamais compartilhe senhas ou códigos de confirmação. Orientações \n" +
+                            "adicionais e práticas de segurança ajudam a manter seus dados e dinheiro protegidos\n" +
+                            "no dia a dia. \n");
+                case 4:
+                    System.out.println("Saindo do programa...");
+                    break;
+                default:
+                    System.out.println("Opção Inválida! Tente novamente.\n");
+            }
         }
     }
 }
